c668ae06e3bfedd1fd6e3c0a3b143821401ae5ca
RahaneSayali <sayalipr15@gmail.com>
2023-12-28 19:15:40 +0530

    another solution added

--- Day17/69. Sqrt(x).cpp
+++ Day17/69. Sqrt(x).cpp
@@ -1,14 +1,52 @@
-class Solution {
+class Solution
+{
 public:
-    int mySqrt(int num) {
-         int high= num,low=0;
-        while(high>low){
+    int mySqrt(int num)
+    {
+        int high = num, low = 0;
+        while (high > low)
+        {
             long int mid = low + ((long int)high - low + 1) / 2;
-            if(mid<=sqrt(num))
+            if (mid <= sqrt(num))
                 low = mid;
             else
-                high = mid -1;
+                high = mid - 1;
         }
         return low;
+    }
+};
+
+// solution 2
+class Solution
+{
+public:
+    long long int binarySearch(int n)
+    {
+        int s = 0;
+        int e = n;
+        int ans = -1;
+        long long int mid = s + (e - s) / 2;
+        while (s <= e)
+        {
+            long long int square = mid * mid;
+            if (square == n)
+                return mid;
+
+            if (square < n)
+            {
+                ans = mid;
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid - 1;
+            }
+            mid = s + (e - s) / 2;
+        }
+        return ans;
+    }
+    int mySqrt(int x)
+    {
+        return binarySearch(x);
     }
 };
