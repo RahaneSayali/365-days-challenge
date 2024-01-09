1ebcb064b6cac1eed287bcff0db85530a6b75b40
RahaneSayali <sayalipr15@gmail.com>
2024-01-09 22:25:50 +0530

    easy

---
+++ Day30/872. Leaf-Similar Trees.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    bool leafSimilar(TreeNode *root1, TreeNode *root2)
+    {
+        function<void(TreeNode *, vector<int> &)> collectLeafValues =
+            [&](TreeNode *root, vector<int> &leafValues)
+        {
+            if (!root)
+            {
+                return;
+            }
+            if (!root->left && !root->right)
+            {
+                leafValues.push_back(root->val);
+            }
+            collectLeafValues(root->left, leafValues);
+            collectLeafValues(root->right, leafValues);
+        };
+
+        vector<int> leafValues1, leafValues2;
+        collectLeafValues(root1, leafValues1);
+        collectLeafValues(root2, leafValues2);
+
+        return leafValues1 == leafValues2;
+    }
+};
