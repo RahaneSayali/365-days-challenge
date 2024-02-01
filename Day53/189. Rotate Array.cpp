b74b05e98536494547ea72d51a4037defa8c6939
RahaneSayali <sayalipr15@gmail.com>
2024-02-01 13:13:05 +0530

    medium formula is imp

---
+++ Day53/189. Rotate Array.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    void rotate(vector<int> &nums, int k)
+    {
+        vector<int> temp(nums.size());
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+
+            temp[(i + k) % nums.size()] = nums[i]; // k=3
+                                                   //[arr(i+k)%n]=arr[i] created formula
+        }
+        nums = temp;
+    }
+};
