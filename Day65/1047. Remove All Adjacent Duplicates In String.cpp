bc2fea2abf341fb903d14e26c575067e9b0aad53
RahaneSayali <sayalipr15@gmail.com>
2024-02-12 12:43:51 +0530

    easy stack approach

---
+++ Day65/1047. Remove All Adjacent Duplicates In String.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int n=s.size(), i=0;
+        stack<char>st;
+
+        while(i<n){
+            if(st.empty()|| st.top()!=s[i])
+            st.push(s[i]);   //insertig element
+
+            else
+            st.pop();
+
+            i++;
+        }
+        string ans="";
+        while(!st.empty()){
+           char ele=st.top();
+           st.pop();
+
+           ans +=ele;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
