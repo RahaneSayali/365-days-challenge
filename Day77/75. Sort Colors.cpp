cd473ce419f07012da0f84783dcf138ce25b5388
RahaneSayali <sayalipr15@gmail.com>
2024-02-25 19:16:34 +0530

    dutch national flag algo used (medium)

---
+++ Day77/75. Sort Colors.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int n = nums.size();
+        int low = 0, mid = 0, high = n - 1;
+        while (mid <= high)
+        {
+            if (nums[mid] == 0)
+            {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
