57606c37bb9788439380e625edf5e86e6f07b89d
RahaneSayali <sayalipr15@gmail.com>
2024-02-25 19:47:51 +0530

    easy (kadane algo)

---
+++ Day77/53. Maximum Subarray.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int sum = 0;
+        int maxi = INT_MIN;
+        for (auto it : nums)
+        { // linear iteration
+            sum += it;
+            maxi = max(sum, maxi);
+            if (sum < 0)
+            {
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+};
