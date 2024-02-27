338e4e3fa05c20a24ed931886955cbc978de75c8
RahaneSayali <sayalipr15@gmail.com>
2024-02-27 21:52:13 +0530

    hard revisit

---
+++ Day79/118. Pascal's Triangle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return (int)(res);
+}
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+          vector<vector<int>> ans;
+    //Store the entire pascal's triangle:
+    for (int row = 1; row <= numRows; row++) {
+        vector<int> tempLst; // temporary list
+        for (int col = 1; col <= row; col++) {
+            tempLst.push_back(nCr(row - 1, col - 1));
+        }
+        ans.push_back(tempLst);
+    }
+    return ans;
+}};
