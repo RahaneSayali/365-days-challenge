e7a2b387b7c9bdbd35f6ec1223abb921dd6001a1
RahaneSayali <sayalipr15@gmail.com>
2024-03-09 15:52:06 +0530

    medium (bs)

---
+++ Day90/81. Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+public:
+    bool search(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target)
+                return true;
+
+            if (nums[low] == nums[mid] && nums[mid] == nums[high])
+            {
+                low++, high--;
+                continue;
+            }
+
+            if (nums[low] <= nums[mid])
+            {
+                if (nums[low] <= target && target <= nums[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            else
+            {
+                if (nums[mid] <= target && target <= nums[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+};
