9a068e47cecebb860a5970148ed1fbbeec6e2e25
RahaneSayali <sayalipr15@gmail.com>
2024-03-29 12:24:58 +0530

    medium recursion

---
+++ Day110/50. Pow(x, n).cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1.0;
+        long long nn=n;
+        if(nn<0)
+        nn=-1*nn;
+        while(nn){
+            if(nn%2){   //odd power
+                ans=ans*x;
+                nn=nn-1;
+            }
+            else{
+                x=x*x;   //even power
+                nn=nn/2;
+            }
+        }
+        if(n<0) ans=(double)(1.0)/(double)(ans); //edge case -ve value of n
+        return ans;
+    }
+};
