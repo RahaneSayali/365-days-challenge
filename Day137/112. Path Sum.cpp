7fc745c342c1f4480f910a2bb30914897c3f1805
RahaneSayali <sayalipr15@gmail.com>
2024-04-25 23:14:42 +0530

    easy

---
+++ Day137/112. Path Sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (!root) {
+            return false;
+        }
+        
+        if (!root->left && !root->right) {
+            return targetSum == root->val;
+        }
+        
+        bool left_sum = hasPathSum(root->left, targetSum - root->val);
+        bool right_sum = hasPathSum(root->right, targetSum - root->val);
+        
+        return left_sum || right_sum;
+    }
+};
