6a4bf273d54e981e9f63655452b3aa69b9433a63
RahaneSayali <sayalipr15@gmail.com>
2024-06-04 21:29:10 +0530

    easy

---
+++ Day177/409. Longest Palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_set<char> charSet;
+         int length = 0;
+         for (char c : s) {
+            // If the character is already in the set, remove it and increase the length by 2
+            if (charSet.find(c) != charSet.end()) {
+                charSet.erase(c);
+                length += 2;
+            } else {
+                // If the character is not in the set, add it to the set
+                charSet.insert(c);
+            }
+             }
+        
+        // If there are any characters left in the set, add 1 to the length for the middle character
+        if (!charSet.empty()) {
+            length += 1;
+        }
+        
+        // Return the total length of the longest palindrome
+        return length;
+    }
+};
