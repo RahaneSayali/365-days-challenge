21eec9ed977816c68bba2a5a133964d7b5182b64
RahaneSayali <sayalipr15@gmail.com>
2024-06-14 22:28:11 +0530

    easy

---
+++ Day187/101. Symmetric Tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isMirror(TreeNode* left, TreeNode* right) {
+    if (!left && !right) return true;
+    if (!left || !right) return false;
+    return (left->val == right->val) && isMirror(left->left, right->right) && isMirror(left->right, right->left);
+}
+
+bool isSymmetric(TreeNode* root) {
+    if (!root) return true;
+    return isMirror(root->left, root->right);
+}
+
+};
