4bca5ab48287dc351d6380bef5c3a357a4f3e53a
RahaneSayali <sayalipr15@gmail.com>
2024-07-05 21:39:40 +0530

    medium

---
+++ Day208/2058. Find the Minimum and Maximum Number of Nodes Between Critical Points.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        vector<int> criticalPoints;
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        int position = 0;
+        
+        while (curr != nullptr && curr->next != nullptr) {
+            if (prev != nullptr && curr->next != nullptr) {
+                if ((curr->val > prev->val && curr->val > curr->next->val) || 
+                    (curr->val < prev->val && curr->val < curr->next->val)) {
+                    criticalPoints.push_back(position);
+                }
+            }
+            prev = curr;
+            curr = curr->next;
+            position++;
+        }
+        
+        if (criticalPoints.size() < 2) {
+            return {-1, -1};
+        }
+        
+        int minDistance = INT_MAX;
+        int maxDistance = criticalPoints.back() - criticalPoints.front();
+        
+        for (int i = 1; i < criticalPoints.size(); i++) {
+            minDistance = min(minDistance, criticalPoints[i] - criticalPoints[i - 1]);
+        }
+        
+        return {minDistance, maxDistance};
+    }
+};
