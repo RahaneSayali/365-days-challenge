5c8d010aeb577b95459b999123caf14193ada5f2
RahaneSayali <sayalipr15@gmail.com>
2024-07-10 21:41:34 +0530

    easy

--- Day213/1598. Crawler Log Folder.cpp
+++ Day213/1598. Crawler Log Folder.cpp
@@ -1,14 +1,21 @@
-class Solution {
+class Solution
+{
 public:
-    int minOperations(vector<string>& logs) {
+    int minOperations(vector<string> &logs)
+    {
         int step = 0;
-for (const auto& log : logs) {
-    if (log == "../") {
-        if (step > 0) step--;
-    } else if (log != "./") {
-        step++;
-    }
-}
-return step;
+        for (const auto &log : logs)
+        {
+            if (log == "../")
+            {
+                if (step > 0)
+                    step--;
+            }
+            else if (log != "./")
+            {
+                step++;
+            }
+        }
+        return step;
     }
 };
