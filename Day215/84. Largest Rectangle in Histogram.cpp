2886c192fc277d519655c8d3a63087a9a7483963
RahaneSayali <sayalipr15@gmail.com>
2024-07-12 15:19:23 +0530

    optimized

--- Day215/84. Largest Rectangle in Histogram.cpp
+++ Day215/84. Largest Rectangle in Histogram.cpp
@@ -45,3 +45,29 @@ public:
     }
 
 };
+
+//optimized
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int maxA=0;
+        int n=heights.size();
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || heights[st.top()] >= heights[i])){
+                int height=heights[st.top()];
+                st.pop();
+                int width;
+                if(st.empty()) width=i;
+                else width= i - st.top() - 1;
+                maxA =max(maxA, width*height);
+
+
+            }
+            st.push(i);
+
+        }
+        return maxA;
+        
+    }
+};
