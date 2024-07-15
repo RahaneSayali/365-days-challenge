1c019924bedb8b2c69d3b349bcda498207ec7d7b
RahaneSayali <sayalipr15@gmail.com>
2024-07-15 21:10:26 +0530

    medium

---
+++ Day218/2196. Create Binary Tree From Descriptions.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        TreeNode* map[100001] = {};
+        bool child[100001] = {};
+        for (auto& d : descriptions){
+            if (map[d[0]] == nullptr) map[d[0]] = new TreeNode(d[0]);
+            TreeNode* node = (map[d[1]] == nullptr ? new TreeNode(d[1]) : map[d[1]]);
+            if (d[2])
+                map[d[0]]->left = node;
+            else
+                map[d[0]]->right = node;
+            map[node->val] = node;
+            child[d[1]] = true;
+        }
+        for (auto& d : descriptions)
+            if (!child[d[0]])
+                return map[d[0]];
+        return nullptr;
+    }
+};
