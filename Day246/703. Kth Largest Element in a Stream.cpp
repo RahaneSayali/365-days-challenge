4adff7d1c45aa3fc6f35e39dcd544b61b0a34459
RahaneSayali <sayalipr15@gmail.com>
2024-08-12 22:33:52 +0530

    medium heap

---
+++ Day246/703. Kth Largest Element in a Stream.cpp
@@ -0,0 +1,29 @@
+class KthLargest {
+public:
+    KthLargest(int k, vector<int>& nums) : k(k) {
+        for (int num : nums) {
+            if (minHeap.size() < k) {
+                minHeap.push(num);
+            } else if (num > minHeap.top()) {
+                minHeap.push(num);
+                if (minHeap.size() > k) {
+                    minHeap.pop();
+                }
+            }
+        }
+    }
+
+    int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.push(val);
+        } else if (val > minHeap.top()) {
+            minHeap.push(val);
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+
+private:
+    int k;
+    priority_queue<int, vector<int>, greater<int>> minHeap;  // Min-heap to store the k largest elements
+};
