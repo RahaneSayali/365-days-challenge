679cbe478f9bc442d4683a660db65dd1c21f919c
RahaneSayali <sayalipr15@gmail.com>
2024-08-12 22:22:59 +0530

    easy

---
+++ Day246/1470. Shuffle the Array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int>result;
+        for(int i=0;i<n;i++){
+            result.push_back(nums[i]);
+            result.push_back(nums[i+n]);
+        }
+        return result;
+    }
+};
